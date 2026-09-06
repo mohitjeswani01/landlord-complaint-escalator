e4f083837733ed3e11c53b24bdc0f2b3930444af
Mohit Jeswani <mohitjeswani74@gmail.com>
2026-09-06 16:54:54 +0000

    chore: finalize repository state

--- main.py
+++ main.py
@@ -20,7 +20,10 @@ root_dir = os.path.dirname(os.path.abspath(__file__))
 if root_dir not in sys.path:
     sys.path.insert(0, root_dir)
 
-from play.main import main
+try:
+    from lib.main import main
+except ImportError:
+    from play.main import main
 
 if __name__ == "__main__":
     main()
